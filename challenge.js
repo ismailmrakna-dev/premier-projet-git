cabfff95fec3ea4ba74ab5258dc95c0422254225
John Doe <mraknaismail448@gmail.com>
2026-09-16 17:23:36 +0200

    first step to JavaScript

---
+++ challenge.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+const name= prompt('enter your name');
+const age = prompt('Enter your age');
+console.log(`Your name is ${name}! You have ${age} years old`)
